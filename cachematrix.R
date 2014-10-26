5ca44a3e9243f7560f35449a2dde8885c40b4776
sslo <shunut78@gmail.com>
2014-10-26 18:24:10 -0400

    Slo Assignment2 code
    Code to compute and store in cache, the inverse of a square matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function takes a matrix as an input
+## to create a new matrix with the same elements
+## of the input matrix while allowing for
+## it's inverse to be store and retrived
+## multiple times without computing the inverse again. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)  
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the
+## special matrix if it hasn't already being computed
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()                     #get inverse
+  if(!is.null(m)) {
+    message("inverse from cached data")
+    return(m) }
+  data <- x$get()                     # read matrix elements
+  m <- solve(data, ...)               # compute if inverse has not being computed
+  x$setinv(m)                         # store inverse in the cache
+  m
+  
 }
